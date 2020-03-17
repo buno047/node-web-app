cdb498e8a42d3e68f3cca018a965f9ff7c0a5dd4
Shib Dhar <shib047@gmail.com>
2020-03-17 22:04:21 +0600

    ci updated

--- app/server.js
+++ app/server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World.. I am from Docker running in kubernetesssss');
+  res.send('Hello World.. I am from Dockerrrrrrrrrrrrrrrr running in kubernetesssss');
 });
 
 app.listen(PORT, HOST);
